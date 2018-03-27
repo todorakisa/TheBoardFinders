0d5b47b0a631cf0014d0e62b37c3c4b5b9fd672d
todorakisa <todor.domitrov2000@gmail.com>
2018-03-27 17:11:37 +0300

    register

--- BoardFind/src/main/java/com/example/app/Controllers/UsersController.java
+++ BoardFind/src/main/java/com/example/app/Controllers/UsersController.java
@@ -1,15 +1,22 @@
 package com.example.app.Controllers;
 
+import com.example.app.model.UserCreateRequestModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UsersController {
-    @RequestMapping(value = "/users/register", method = RequestMethod.GET)
 
+    @RequestMapping(value = "/users/register", method = RequestMethod.GET)
     public String register()
     {
+        return "users/register";
+    }
+
+    @PostMapping("users/register")
+    public String register(UserCreateRequestModel model){
         return "users/register";
     }
 }
---
+++ BoardFind/src/main/java/com/example/app/model/UserCreateRequestModel.java
@@ -0,0 +1,22 @@
+package com.example.app.model;
+
+public class UserCreateRequestModel {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
