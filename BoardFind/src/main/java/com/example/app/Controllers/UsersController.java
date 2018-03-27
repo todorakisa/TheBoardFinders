27470310c08559172d398b8168c9d0ffb2defebd
IliyanTachev <iliqn.tachev@gmail.com>
2018-03-27 16:47:30 +0300

    added register

---
+++ BoardFind/src/main/java/com/example/app/Controllers/UsersController.java
@@ -0,0 +1,14 @@
+package com.example.app.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UsersController {
+    @RequestMapping(value = "/users/register", method = RequestMethod.GET)
+    public String register()
+    {
+        return "users/register";
+    }
+}
