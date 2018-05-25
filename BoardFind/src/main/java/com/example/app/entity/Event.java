9052c0c95471337c48ba4fbabd5ccfd8af8438e6
todorakisa <todor.domitrov2000@gmail.com>
2018-05-25 14:05:44 +0300

    added description

--- BoardFind/src/main/java/com/example/app/entity/Event.java
+++ BoardFind/src/main/java/com/example/app/entity/Event.java
@@ -16,11 +16,19 @@ public class Event {
     private List<User> players;
     private double latitude;
     private double longitude;
+    private String description;
+
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
 
     public double getLatitude(){
         return latitude;
     }
-    public void setLatitude( double latitude){
+    public void setLatitude(double latitude){
         this.latitude = latitude;
     }
 
