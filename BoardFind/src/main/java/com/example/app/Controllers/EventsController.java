61496c2d4a10d17f8d929aec1ee1bf0e420d666a
IliyanTachev <iliqn.tachev@gmail.com>
2018-05-27 22:46:47 +0300

    added owner field

--- BoardFind/src/main/java/com/example/app/Controllers/EventsController.java
+++ BoardFind/src/main/java/com/example/app/Controllers/EventsController.java
@@ -3,6 +3,8 @@ package com.example.app.Controllers;
 import com.example.app.Service.EventService;
 import com.example.app.model.RegistrationEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
--- BoardFind/src/main/java/com/example/app/ServiceImpl/EventServiceImpl.java
+++ BoardFind/src/main/java/com/example/app/ServiceImpl/EventServiceImpl.java
@@ -1,14 +1,20 @@
 package com.example.app.ServiceImpl;
 import com.example.app.Repository.EventRepository;
+import com.example.app.Repository.UserRepository;
 import com.example.app.Service.EventService;
 import com.example.app.entity.Event;
 import com.example.app.model.RegistrationEvent;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventServiceImpl implements EventService {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private EventRepository eventRepository;
@@ -22,6 +28,9 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public void register(RegistrationEvent registrationEvent) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+
         Event event = this.modelMapper.map(registrationEvent, Event.class);
         event.setGames(registrationEvent.getGamesToParse());
         event.setDescription(registrationEvent.getDescription());
@@ -30,6 +39,7 @@ public class EventServiceImpl implements EventService {
         event.setName(registrationEvent.getName());
         event.setPlayers(registrationEvent.getPlayers());
         event.setDate(registrationEvent.getDate());
+        event.setOwner(this.userRepository.findOneByUsername(name));
         this.eventRepository.save(event);
     }
 }
