ce4a6c963b62c3cc270a2b2aed34e6723304037f
todorakisa <todor.domitrov2000@gmail.com>
2018-05-30 01:32:27 +0300

    problem with Event Repository

--- BoardFind/src/main/java/com/example/app/repository/EventRepository.java
+++ BoardFind/src/main/java/com/example/app/repository/EventRepository.java
@@ -1,10 +1,15 @@
 package com.example.app.repository;
 import com.example.app.entity.Event;
+import com.example.app.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
-    Event findOneByName(String name);
+//    @Query("SELECT e FROM events e WHERE LOWER(e.name) = LOWER(:name)")
+//    Event findOneByName(@Param("name") String name);
+
     List<Event> findAll();
 }
