e146b777b05a2796207129c9ec858ba3b71eb1f1
todorakisa <todor.domitrov2000@gmail.com>
2018-05-29 11:59:59 +0300

    merge

--- BoardFind/src/main/java/com/example/app/Controllers/EventsController.java
+++ BoardFind/src/main/java/com/example/app/Controllers/EventsController.java
@@ -1,15 +1,15 @@
 package com.example.app.Controllers;
 
-import com.example.app.Service.EventService;
-import com.example.app.model.RegistrationEvent;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+        import com.example.app.Service.EventService;
+        import com.example.app.model.RegistrationEvent;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.security.core.Authentication;
+        import org.springframework.security.core.context.SecurityContextHolder;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+        import javax.validation.Valid;
 
 @Controller
 public class EventsController {
@@ -19,8 +19,8 @@ public class EventsController {
 
     @GetMapping("/user/eventsAndPeople/registerEvent")
     public String getRegisterPage(Model model, RegistrationEvent registrationEvent){
-            model.addAttribute("event", registrationEvent);
-            return "events/registerEvent";
+        model.addAttribute("event", registrationEvent);
+        return "events/registerEvent";
     }
 
     @PostMapping("/user/eventsAndPeople/registerEvent")
